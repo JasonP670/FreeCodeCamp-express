8caf91acabded3d7cfc51fa510f3a64b218bfc6e
Jason <jasonp670@gmail.com>
2021-11-11 21:11:05 -0700

    FCC 2-4

--- myApp.js
+++ myApp.js
@@ -6,4 +6,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
+});
+
 module.exports = app;
